8e44a4ae14e5458333a4e791f786a3a348323563
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-02-11 16:38:50 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import { TextFormWithHistory } from './components/TextFormWithHistory';
+
+function App() {
+	return (
+		<div className="App">
+			<TextFormWithHistory/>
+			<TextFormWithHistory/>
+		</div>
+	);
+}
+
+export default App;
