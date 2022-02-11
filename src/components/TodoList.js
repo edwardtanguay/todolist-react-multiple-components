46ac81c9f90579d36d4e6c5ce41916c75a1115a0
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-02-11 22:00:52 +0100

    scrolling in

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -7,23 +7,27 @@ export const TodoList = () => {
 	const [todo, setTodo] = useState('');
 	const [todoList, setTodoList] = useState(_todoList === null ? [] : _todoList);
 
-	const handleButtonClick = () => {
+	const handleButtonClick = (e) => {
+		e.preventDefault();
 		todoList.push(`${todo} (${area})`);
 		localStorage.setItem('todoList', JSON.stringify(todoList));
 		setTodoList([...todoList]);
 	}
 
 	return (
-		<div>
+		<div className="todoListComponent">
 			<form>
 				<input id="home" type="radio" name="area" value="home" checked={area === "home"} onChange={(e) => setArea(e.target.value)} />
 				<label htmlFor="home">Home</label>
 
 				<input id="work" type="radio" name="area" value="work" checked={area === "work"} onChange={(e) => setArea(e.target.value)} />
 				<label htmlFor="work">Work</label>
+
+				<div className="inputArea">
+					<input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
+					<button onClick={(e) => handleButtonClick(e)}>Add Todo</button>
+				</div>
 			</form>
-			<input type="todo" value={todo} onChange={(e) => setTodo(e.target.value)} />
-			<button onClick={(e) => handleButtonClick(e)}>Add Todo</button>
 			<div className="todoList">
 				{todoList.map((m, i) => <div key={i}>{m}</div>)}
 			</div>
